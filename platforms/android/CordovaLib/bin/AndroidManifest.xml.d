c0b518aeb16a59b18475655a18c43303af634d48
Ben Stewart <ben@eyescreamdesign.com>
2014-10-02 11:54:01 -0500

    deleted files as prep for 1.2

--- platforms/android/CordovaLib/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Ben\Cordova_Projects\translation_studio_1.2\platforms\android\CordovaLib\bin\AndroidManifest.xml : \
-C:\Users\Ben\Cordova_Projects\translation_studio_1.2\platforms\android\CordovaLib\AndroidManifest.xml \
